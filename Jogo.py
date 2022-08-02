ccd0049b60407f79bea766a5fc9b94dc57539fdd
MarinaLebl <marinabplebl@gmail.com>
2022-08-01 21:15:50 -0300

    tela

--- Jogo.py
+++ Jogo.py
@@ -1,1 +1,9 @@
+import pygame
+pygame.init()
 
+#Configuração da tela do jogo
+tela_tamanho=(750, 750)
+tela=pygame.display.set_mode(tela_tamanho)
+pygame.display.set_caption("Jogo da cobrinha")
+branco=(255, 255, 255)
+tela.fill(branco)
