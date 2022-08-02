fed08118b3c09ef98d438d6c6b7ea9a835219727
MarinaLebl <marinabplebl@gmail.com>
2022-08-02 09:36:18 -0300

    imagens

--- Jogo.py
+++ Jogo.py
@@ -1,9 +1,24 @@
 import pygame
+import random
 pygame.init()
 
 #Configuração da tela do jogo
 tela_tamanho=(750, 750)
 tela=pygame.display.set_mode(tela_tamanho)
-pygame.display.set_caption("Jogo da cobrinha")
-branco=(255, 255, 255)
-tela.fill(branco)
+pygame.display.set_caption("Doodle Jump")
+tela.fill(0, 0, 255)
+pygame.disply.flip()
+timer=pygame.time.Clock()
+self.running=True
+self.show=True
+
+score=0
+max_score=0
+saved_max_sore=0
+game_over= False
+
+for event in pygame.event.get():
+    if event.key == pygame.K_SPACE and game_over:
+        game_over=False
+        score=0
+        
