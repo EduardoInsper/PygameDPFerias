8a7cfec584b3984c0797cca4899a7cadacb6f8bb
Han_Solo <103543064+EduardoInsper@users.noreply.github.com>
2022-08-03 11:41:38 -0300

    menu

--- main_f/telas.py
+++ main_f/telas.py
@@ -1,17 +1,17 @@
 import pygame
   
 pygame.init()
+bg = pygame.image.load("tela_inicial.jpg")
+bg = pygame.transform.scale(bg, (400, 500))
 largura = 400
 altura = 500
 tela = pygame.display.set_mode((largura, altura))
 cor_clara = (170,170,170)
 cor_escura = (100,100,100)
 roxo = (100,0,100)
+fonte_p = pygame.font.Font('aladdin.ttf', 27)
+branco = (255,255,255)
 menu = True
-#text = GC.fonte_p.render('Sair' , True , GC.branco)
-smallfont = pygame.font.SysFont('Corbel',35)
-color = (255,255,255)
-text = smallfont.render('quit' , True , color)
   
 while menu:
       
@@ -21,22 +21,26 @@ while menu:
             menu = False
               
         if ev.type == pygame.MOUSEBUTTONDOWN:
-              
             if largura/2 <= mouse[0] <= largura/2+140 and altura/2 <= mouse[1] <= altura/2+40:
                 menu = False
-                  
-    tela.fill(roxo)
-      
+            if largura/6 <= mouse[0] <= largura/6+140 and altura/2 <= mouse[1] <= altura/2+40:
+                menu = False
+    tela.blit(bg, (0,0))
     mouse = pygame.mouse.get_pos()
+
+    if largura/6 <= mouse[0] <= largura/6-10 and altura/2 <= mouse[1] <= altura/2+40:
+        pygame.draw.rect(tela, cor_clara, (largura/6-10, altura/2, 140, 40))
+    else:
+        pygame.draw.rect(tela, cor_escura, (largura/6-10, altura/2, 140, 40))
       
     if largura/2 <= mouse[0] <= largura/2+140 and altura/2 <= mouse[1] <= altura/2+40:
-        pygame.draw.rect(tela,cor_clara,[largura/2,altura/2,140,40])
-          
+        pygame.draw.rect(tela,cor_clara,[largura/2,altura/2,140,40])      
     else:
         pygame.draw.rect(tela,cor_escura,[largura/2,altura/2,140,40])
       
 
-    tela.blit(text , (largura/2+50,altura/2))
+    tela.blit(fonte_p.render('Sair' , True , branco) , (largura/2+40,altura/2))
+    tela.blit(fonte_p.render('Jogar' , True , branco) , (largura/6+40,altura/2))
       
     pygame.display.update()
 pygame.quit()
