01a189139c44c0d63cad355c5a39b0176095520c
Han_Solo <103543064+EduardoInsper@users.noreply.github.com>
2022-08-03 11:09:36 -0300

    telas

--- main_f/telas.py
+++ main_f/telas.py
@@ -1,28 +1,29 @@
 import pygame
-from GC import tela, altura, largura
   
 pygame.init()
-
+largura = 400
+altura = 500
+tela = pygame.display.set_mode((largura, altura))
 cor_clara = (170,170,170)
 cor_escura = (100,100,100)
 roxo = (100,0,100)
-  
+menu = True
 #text = GC.fonte_p.render('Sair' , True , GC.branco)
 smallfont = pygame.font.SysFont('Corbel',35)
 color = (255,255,255)
 text = smallfont.render('quit' , True , color)
   
-while True:
+while menu:
       
     for ev in pygame.event.get():
           
         if ev.type == pygame.QUIT:
-            pygame.quit()
+            menu = False
               
         if ev.type == pygame.MOUSEBUTTONDOWN:
               
             if largura/2 <= mouse[0] <= largura/2+140 and altura/2 <= mouse[1] <= altura/2+40:
-                pygame.quit()
+                menu = False
                   
     tela.fill(roxo)
       
@@ -38,3 +39,4 @@ while True:
     tela.blit(text , (largura/2+50,altura/2))
       
     pygame.display.update()
+pygame.quit()
