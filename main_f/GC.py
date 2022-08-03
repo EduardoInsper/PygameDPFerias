303b70a2140d8f6e94145464041642a6556847a5
Han_Solo <103543064+EduardoInsper@users.noreply.github.com>
2022-08-03 08:56:37 -0300

    nomes e teste

--- main_f/GC.py
+++ main_f/GC.py
@@ -1,5 +1,29 @@
+import pygame
+pygame.init()
+
 branco = (255, 255, 255)
 preto = (0, 0, 0)
+azul = (0, 0, 255)
 cinza = (100, 100, 100)
 largura = 400
 altura = 500
+jogador_img = pygame.image.load("spritekkkk.png")
+fps = 60
+
+fonte_p = pygame.font.Font('aladdin.ttf', 16)
+fonte_g = pygame.font.Font('aladdin.ttf', 32)
+
+
+timer = pygame.time.Clock()
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Aladdin")
+
+rodando = True
+while rodando:
+    timer.tick(fps)
+    tela.fill(azul)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            rodando = False
+    pygame.display.flip()
+pygame.quit()
