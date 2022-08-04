e1a71fefa7b11e7cced2919ab29874f48642ea21
Han_Solo <103543064+EduardoInsper@users.noreply.github.com>
2022-08-04 10:47:15 -0300

    arrumando colisao

--- main_f/GC.py
+++ main_f/GC.py
@@ -11,7 +11,9 @@ preto = (0, 0, 0)
 azul = (0, 0, 255)
 cinza = (100, 100, 100)
 background= azul
-jogador_img = pygame.transform.scale(pygame.image.load("spritekkkk.png"), (90, 70))
+#jogador_img = pygame.transform.scale(pygame.image.load("spritekkkk.png"), (90, 70))
+jogador_idle = pygame.image.load("alladin_jogador/sprite_alladinF00.png")
+tapete = pygame.image.load("tapete/sprite_tapete0.png")
 fps = 60
 fonte_p = pygame.font.Font("aladdin.ttf", 25)
 fonte_pontos = pygame.font.Font("freesansbold.ttf", 16)
@@ -26,13 +28,14 @@ pygame.display.set_caption("Aladdin")
 rodando = False
 
 #variaveis
-jogador_x = 170
+jogador_x = 180
 jogador_y = 400
 plataformas=[[175, 480, 70, 10], [85, 370, 70, 10], [265, 370, 70, 10],[175, 260, 70, 10], [85, 150, 70, 10], [265, 150, 70, 10], [175, 40, 70, 10]]
 jump= False
 y_change= 0 
 x_change=0
 velojogador=3
+game_over=False
 
 #Pontuação
 score=0
@@ -78,7 +81,7 @@ def caso_colisao(rect_list, j):
     global jogador_y
     global y_change
     for i in range(len(rect_list)):
-        if rect_list[i].colliderect([jogador_x+20, jogador_y + 60, 90, 5]) and jump==False and y_change>0:
+        if rect_list[i].colliderect([jogador_x-2, jogador_y + 40, 90, 5]) and jump==False and y_change>0:
             j=True
     return j
 
@@ -98,7 +101,7 @@ while rodando == False:
 while rodando:
     timer.tick(fps)
     tela.fill(background)
-    tela.blit(jogador_img, (jogador_x, jogador_y))
+    tela.blit(jogador_idle, (jogador_x, jogador_y))
     blocos= []
     #Textos de pontuação
     texto_pontuacao= fonte_pontos.render("Score: "+  str(score), True, branco, background )
@@ -155,9 +158,9 @@ while rodando:
         jogador_x=330
     # Espelha personagem de acordo com sentido do movimento
     if x_change >0:
-        jogador_img = pygame.transform.scale(pygame.image.load("spritekkkk.png"), (90, 70))
+        jogador_idle = pygame.image.load("alladin_jogador/sprite_alladinF00.png")
     elif x_change<0:
-        jogador_img = pygame.transform.flip(pygame.transform.scale(pygame.image.load("spritekkkk.png"), (90, 70)), 1, 0)
+        jogador_idle = pygame.transform.flip(pygame.image.load("alladin_jogador/sprite_alladinF00.png"), 1, 0)
     #Contabiliza high score
     if score>max_score:
         max_score=score
